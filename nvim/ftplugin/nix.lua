904de5db481b80dd6d2b72fe327f21b3808c3c5a
Niko Birbilis <nikob381@gmail.com>
2024-12-27 16:58:34 -0500

    add format on save for nix files

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -15,4 +15,25 @@ vim.lsp.start {
   cmd = { 'nil' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
+  on_attach = function(client, bufnr)
+    -- Enable formatting on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        local params = {
+          textDocument = {
+            uri = vim.uri_from_bufnr(bufnr)
+          }
+        }
+        client.request('textDocument/formatting', params, nil, bufnr)
+      end,
+    })
+  end,
+  settings = {
+    ['nil'] = {
+      formatting = {
+        command = { "nixpkgs-fmt" }
+      }
+    }
+  }
 }
